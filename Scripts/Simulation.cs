171ff00806f717f3d55fa8061b9289d6e70b9da3
Harald Scheirich <hscheirich@yahoo.com>
2024-10-29 11:16:06 -0400

    Adding data json, reading it and setting it in the tile

---
+++ Scripts/Simulation.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class Simulation : Node
+{
+    [Export] public World World;
+
+    public override void _Process(double delta)
+    {
+        
+    }
+}
